3339f925291ef8ab7b7cb0c222b895eb176d6d8c
Vladislav Stalmakhovich <VLs125.25@gmail.com>
2020-08-26 23:41:31 +1000

    fix git

---
+++ src/Components/Top/Top.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import {AppBar, Box, Container, IconButton, Toolbar} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import {useStyles} from "../../styles";
+import TemporaryDrawer from "./TopMenu/TopMenu";
+import TopMenu from "./TopMenu/TopMenu";
+
+const Top = ()=>{
+    const classes = useStyles();
+    return(
+        <AppBar color='white' position='fixed'>
+            <Container fixed>
+                <div className={classes.overlay}/>
+                <Toolbar>
+                    <IconButton className={classes.menuButton} edge='start' color='inherit' aria-label='menu'>
+                        <TopMenu/>
+                    </IconButton>
+                    <img className={classes.logo} src="https://i.ibb.co/PMySjcG/dvssp-Logo.png"/>
+                    <Typography className={classes.title} variant='h6'>Дальневосточная ассоциация
+                        системных семейных психологов</Typography>
+                    <Box mr={3}>
+                        <Button variant='outlined' color='inherit'>
+                            Log in
+                        </Button>
+                    </Box>
+                    <Button mr={3} variant='contained' color='secondary'>
+                        Sign up
+                    </Button>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
+}
+
+export default Top
